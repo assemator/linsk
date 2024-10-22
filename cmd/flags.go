6350c671167948b9042eaa32be7ac295a45c24f9
assemator <48603678+assemator@users.noreply.github.com>
2024-10-22 23:54:54 +0300

    Update flags.go

--- cmd/flags.go
+++ cmd/flags.go
@@ -51,12 +51,16 @@ var (
 
 	// These are to be initialized (set) by the initVMRuntimeFlags function.
 	vmRuntimeLUKSContainerDevice string
+
+	// custom afp password
+	afpPasswordFlag string
 )
 
 func initVMRuntimeFlags(flags *pflag.FlagSet) {
 	flags.StringVar(&vmRuntimeLUKSContainerFlag, "luks-container", "", `Specifies a device path (without "dev/" prefix) to preopen as a LUKS container (password will be prompted). Useful for accessing LVM partitions behind LUKS.`)
 	flags.BoolVarP(&vmRuntimeLUKSContainerEntireDriveFlag, "luks-container-entire-drive", "c", false, `Similar to --luks-container, but this assumes that the entire passed-through volume is a LUKS container (password will be prompted).`)
 	flags.BoolVar(&vmRuntimeInternalAllowLUKSLowMemoryFlag, "allow-luks-low-memory", false, "Allow VM memory allocation lower than 2048 MiB when LUKS is enabled.")
+	flags.StringVar(&afpPasswordFlag, "afp-password", "", "Specifies a static password for AFP sharing. If not provided, a dynamic password will be generated.")
 }
 
 func configureVMRuntimeFlags() {
